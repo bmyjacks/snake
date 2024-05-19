0ad30ee9c60d0a314b984807aa75dc9a0f115ae7
bmyjacks <dev@bmyjacks.io>
2024-05-19 09:56:11 +0800

    update naming

--- logic.py
+++ logic.py
@@ -7,89 +7,93 @@ from config import Config
 
 class SnakeLogic:
     def __init__(self, ui):
-        self.ui = ui
-        self.config = Config()
+        self._ui = ui
+        self._config = Config()
 
-        self.speed = self.config.getint('Game', 'Speed')
-        self.finished = False
-        self.rng = np.random.default_rng(self.config.getint('Game', 'FoodGenerateSeed'))
+        self._speed = self._config.getint('Game', 'Speed')
+        self._finished = False
+        self._rng = np.random.default_rng(self._config.getint('Game', 'FoodGenerateSeed'))
 
-        self.start_time = time.time()
-        self.dx, self.dy = 1, 0  # Initial direction and speed of each axis
-        self.board_size = Config().getint('Game', 'BoardSize')
-        self.snake = np.vstack([[self.board_size // 2, self.board_size // 2],
-                                [self.board_size // 2 - 1, self.board_size // 2],
-                                [self.board_size // 2 - 2, self.board_size // 2]], dtype=int)
-        self.food = list()
+        self._start_time = time.time()
+        self._dx, self._dy = 1, 0  # Initial direction and speed of each axis
+        self._board_size = Config().getint('Game', 'BoardSize')
+        self._snake = np.vstack([[self._board_size // 2, self._board_size // 2],
+                                 [self._board_size // 2 - 1, self._board_size // 2],
+                                 [self._board_size // 2 - 2, self._board_size // 2]], dtype=int)
+        self._food = list()
 
         self.new_food()
         self.move_snake()
 
     def get_duration(self):
-        return time.time() - self.start_time
+        return time.time() - self._start_time
 
     def get_snake(self):
-        return self.snake
+        return self._snake
 
     def get_score(self):
-        return len(self.snake) - 3
+        return len(self._snake) - 3
 
     def is_snake(self, x, y):
-        return (self.snake == [x, y]).all(axis=1).any()
+        return (self._snake == [x, y]).all(axis=1).any()
 
     def judge(self):
-        head = self.snake[0]
-        body = self.snake[1:]
-        if head[0] < 0 or head[0] >= self.board_size or head[1] < 0 or head[1] >= self.board_size:
-            self.finished = True
+        head = self._snake[0]
+        body = self._snake[1:]
+        if head[0] < 0 or head[0] >= self._board_size or head[1] < 0 or head[1] >= self._board_size:
+            self._finished = True
         if (head == body).all(axis=1).any():
-            self.finished = True
+            self._finished = True
 
     def get_finished(self):
-        return self.finished
+        return self._finished
 
     def longer_snake(self):
-        last_1 = self.snake[-1]
-        last_2 = self.snake[-2]
+        last_1 = self._snake[-1]
+        last_2 = self._snake[-2]
 
         new_cell = last_1 + (last_1 - last_2)
-        self.snake = np.vstack([self.snake, new_cell])
+        self._snake = np.vstack([self._snake, new_cell])
 
     def move_snake(self):
-        head = self.snake[0]
-        new_head = head + [self.dx, self.dy]
+        head = self._snake[0]
+        new_head = head + [self._dx, self._dy]
 
-        if (new_head == self.food[0]).all():
+        if (new_head == self._food[0]).all():
             self.new_food()
             self.longer_snake()
 
-        self.snake = np.vstack([new_head, self.snake[:-1]])
+        self._snake = np.vstack([new_head, self._snake[:-1]])
         self.judge()
 
-        if not self.finished:
-            self.ui.after(1000 // self.speed, self.move_snake)
+        if not self._finished:
+            self._ui.after(1000 // self._speed, self.move_snake)
 
     def get_food(self):
-        return self.food
+        return self._food
 
     def new_food(self):
-        self.food = self.food[1:]
-        new_food = [self.rng.integers(0, self.board_size), self.rng.integers(0, self.board_size)]
+        self._food = self._food[1:]
+        new_food = [self._rng.integers(0, self._board_size), self._rng.integers(0, self._board_size)]
         while self.is_snake(*new_food):
-            new_food = [self.rng.integers(0, self.board_size), self.rng.integers(0, self.board_size)]
-        self.food.append(new_food)
+            new_food = [self._rng.integers(0, self._board_size), self._rng.integers(0, self._board_size)]
+        self._food.append(new_food)
 
     def change_direction(self, new_dx, new_dy):
-        self.dx, self.dy = new_dx, new_dy
+        self._dx, self._dy = new_dx, new_dy
 
     def move_up(self):
-        self.change_direction(0, -1)
+        if self._dy == 0:
+            self.change_direction(0, -1)
 
     def move_down(self):
-        self.change_direction(0, 1)
+        if self._dy == 0:
+            self.change_direction(0, 1)
 
     def move_left(self):
-        self.change_direction(-1, 0)
+        if self._dx == 0:
+            self.change_direction(-1, 0)
 
     def move_right(self):
-        self.change_direction(1, 0)
+        if self._dx == 0:
+            self.change_direction(1, 0)
--- ui.py
+++ ui.py
@@ -7,41 +7,41 @@ from logic import SnakeLogic
 class Snake(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.config = Config()
-        self.board_size = self.config.getint('Game', 'BoardSize')
-        self.cell_size_x = self.board_size
-        self.cell_size_y = self.board_size
-        self.update_speed = self.config.getint('Game', 'Speed')
+        self._config = Config()
+        self._board_size = self._config.getint('Game', 'BoardSize')
+        self._cell_size_x = self._board_size
+        self._cell_size_y = self._board_size
+        self._update_speed = self._config.getint('Game', 'Speed')
 
-        self.title(self.config.get('UI', 'Title'))
-        self.geometry(self.config.get('UI', 'Size'))
-        self.resizable(self.config.getboolean('UI', 'Resizable'), self.config.getboolean('UI', 'Resizable'))
+        self.title(self._config.get('UI', 'Title'))
+        self.geometry(self._config.get('UI', 'Size'))
+        self.resizable(self._config.getboolean('UI', 'Resizable'), self._config.getboolean('UI', 'Resizable'))
 
-        self.right_menu = tk.Frame(self, width=100)
-        self.duration_label = tk.Label(self.right_menu, text="Duration:")
-        self.duration_label.pack()
-        self.duration_text = tk.Label(self.right_menu, text="0.00 seconds")
-        self.duration_text.pack()
-        self.score_label = tk.Label(self.right_menu, text="Score:")
-        self.score_label.pack()
-        self.score_text = tk.Label(self.right_menu, text="0")
-        self.score_text.pack()
-        self.quit_button = tk.Button(self.right_menu, text="Quit", command=self.quit)
-        self.quit_button.pack()
-        self.right_menu.pack(side=tk.RIGHT, fill=tk.Y, expand=True, padx=10, pady=10)
+        self._right_menu = tk.Frame(self, width=100)
+        self._duration_label = tk.Label(self._right_menu, text="Duration:")
+        self._duration_label.pack()
+        self._duration_text = tk.Label(self._right_menu, text="0.00 seconds")
+        self._duration_text.pack()
+        self._score_label = tk.Label(self._right_menu, text="Score:")
+        self._score_label.pack()
+        self._score_text = tk.Label(self._right_menu, text="0")
+        self._score_text.pack()
+        self._quit_button = tk.Button(self._right_menu, text="Quit", command=self.quit)
+        self._quit_button.pack()
+        self._right_menu.pack(side=tk.RIGHT, fill=tk.Y, expand=True, padx=10, pady=10)
 
-        self.board = tk.Frame(self, bg='black')
-        self.canvas = tk.Canvas(self.board, bg='black')
-        self.canvas.pack()
-        self.board.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
+        self._board = tk.Frame(self, bg='black')
+        self._canvas = tk.Canvas(self._board, bg='black')
+        self._canvas.pack()
+        self._board.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
 
-        self.logic = SnakeLogic(self)
+        self._logic = SnakeLogic(self)
 
         self.bind('<Configure>', self.update_layout)
-        self.bind('<Up>', lambda e: self.logic.move_up())
-        self.bind('<Down>', lambda e: self.logic.move_down())
-        self.bind('<Left>', lambda e: self.logic.move_left())
-        self.bind('<Right>', lambda e: self.logic.move_right())
+        self.bind('<Up>', lambda e: self._logic.move_up())
+        self.bind('<Down>', lambda e: self._logic.move_down())
+        self.bind('<Left>', lambda e: self._logic.move_left())
+        self.bind('<Right>', lambda e: self._logic.move_right())
 
         self.update_duration()
         self.update_score()
@@ -49,49 +49,49 @@ class Snake(tk.Tk):
         self.draw_food()
 
     def update_layout(self, event):
-        self.canvas.config(width=self.board.winfo_width(), height=self.board.winfo_height())
-        self.cell_size_x = self.canvas.winfo_width() // self.board_size
-        self.cell_size_y = self.canvas.winfo_height() // self.board_size
+        self._canvas.config(width=self._board.winfo_width(), height=self._board.winfo_height())
+        self._cell_size_x = self._canvas.winfo_width() // self._board_size
+        self._cell_size_y = self._canvas.winfo_height() // self._board_size
 
     def update_duration(self):
-        duration = self.logic.get_duration()
-        self.duration_text.config(text=f"{duration:.2f} seconds")
-        if not self.logic.get_finished():
-            self.after(1000 // self.update_speed, self.update_duration)
+        duration = self._logic.get_duration()
+        self._duration_text.config(text=f"{duration:.2f} seconds")
+        if not self._logic.get_finished():
+            self.after(1000 // self._update_speed, self.update_duration)
 
     def update_score(self):
-        score = self.logic.get_score()
-        self.score_text.config(text=score)
+        score = self._logic.get_score()
+        self._score_text.config(text=score)
 
-        if not self.logic.get_finished():
-            self.after(1000 // self.update_speed, self.update_score)
+        if not self._logic.get_finished():
+            self.after(1000 // self._update_speed, self.update_score)
 
     def get_position(self, cell_x, cell_y):
-        x = cell_x * self.cell_size_x
-        y = cell_y * self.cell_size_y
+        x = cell_x * self._cell_size_x
+        y = cell_y * self._cell_size_y
         return x, y
 
     def draw_food(self):
-        self.canvas.delete('food')
-        food = self.logic.get_food()
+        self._canvas.delete('food')
+        food = self._logic.get_food()
         for cell in food:
             cell_x, cell_y = cell
             x, y = self.get_position(cell_x, cell_y)
-            self.canvas.create_rectangle(x, y, x + self.cell_size_x, y + self.cell_size_y, fill='green', tags='food')
+            self._canvas.create_rectangle(x, y, x + self._cell_size_x, y + self._cell_size_y, fill='green', tags='food')
 
-        if not self.logic.get_finished():
-            self.after(1000 // self.update_speed, self.draw_food)
+        if not self._logic.get_finished():
+            self.after(1000 // self._update_speed, self.draw_food)
 
     def draw_snake(self):
-        self.canvas.delete('snake')
-        snake = self.logic.get_snake()
+        self._canvas.delete('snake')
+        snake = self._logic.get_snake()
         for cell in snake:
             cell_x, cell_y = cell
             x, y = self.get_position(cell_x, cell_y)
-            self.canvas.create_rectangle(x, y, x + self.cell_size_x, y + self.cell_size_y, fill='red', tags='snake')
+            self._canvas.create_rectangle(x, y, x + self._cell_size_x, y + self._cell_size_y, fill='red', tags='snake')
 
-        if self.logic.get_finished():
-            self.canvas.create_text(self.canvas.winfo_width() // 2, self.canvas.winfo_height() // 2, text="Game Over",
-                                    fill='white', font=('Arial', 40))
+        if self._logic.get_finished():
+            self._canvas.create_text(self._canvas.winfo_width() // 2, self._canvas.winfo_height() // 2, text="Game Over",
+                                     fill='white', font=('Arial', 40))
         else:
-            self.after(1000 // self.update_speed, self.draw_snake)
+            self.after(1000 // self._update_speed, self.draw_snake)
